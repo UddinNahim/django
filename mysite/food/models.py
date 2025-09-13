d84bd84a3feeba00b8b6367a018e406fe2058a15
Uddin Nahim <nahim.211902019@gmail.com>
2025-09-13 23:49:22 +0600

    touchup the login,register,add item page

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User 
+from django.urls import reverse
 
 # Create your models here.
 class Item(models.Model):
 
     def __str__(self):
         return self.item_name
-
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
-    item_image = models.CharField(max_length=500,default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2jkIKkkOoCoAwxGGU9_qw0zZEfP8lshgO3Q&s")
+    item_image = models.CharField(max_length=500,default="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Frestaurant%2F&psig=AOvVaw3ZxMvdY--awAGUwglDd4ev&ust=1757693191192000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNiU-sOL0Y8DFQAAAAAdAAAAABAK")
+
+    def get_absolute_url(self):
+        return reverse("food:detail", kwargs={"pk": self.pk})
+    
+
